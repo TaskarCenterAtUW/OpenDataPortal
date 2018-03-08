a72da9bab6d3cbef8f21a3e2d20ddbdd2fff58ff
vaneshsu <vaneshsu@gmail.com>
2018-03-07 19:31:39 -0800

    adding sidebar

--- dashboard/src/Components/Map.jsx
+++ dashboard/src/Components/Map.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from 'mapbox-gl-geocoder';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFuZXNoc3UiLCJhIjoiY2pkamZpbzZ3MW0ycTJ6cmxtNnJhZ2k4ZCJ9.bzJJ_dSQlZefW6kWSSjlzw';
 
@@ -11,6 +12,38 @@ class Map extends Component {
       center: [-122.3035, 47.6553],
       zoom: 14
     });
+
+    this.map.on('load', () => {
+      var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken
+      });
+    
+      this.map.addControl(geocoder);
+      
+      /*this.map.addSource('single-point', {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": []
+        }
+      });
+
+      this.map.addLayer({
+          "id": "point",
+          "source": "single-point",
+          "type": "circle",
+          "paint": {
+              "circle-radius": 10,
+              "circle-color": "red"
+          }
+      });
+
+      geocoder.on('result', function(ev) {
+          this.map.getSource('single-point').setData(ev.result.geometry);
+      });*/
+
+      this.map.addControl(new mapboxgl.NavigationControl());
+    });
   }
   
   componentWillUnmount() {
@@ -20,9 +53,10 @@ class Map extends Component {
   render() {
     const style = {
       position: 'absolute',
-      top: 150,
+      top: 0,
       bottom: 0,
-      width: '100%'
+      left: '20%',
+      width: '80vw'
     };
   
     return (
---
+++ dashboard/src/Components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+class Sidebar extends Component {
+      render() {
+        return (
+            <div id="sidebar">
+                <div className="container"> 
+                    <div id="instructions">
+                        Weight each concern (1 - only concern, 0 - not concerned)
+                    </div>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <label>Crime</label>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="0.5" />
+                        </div>
+                        <div>
+                            <label>Transportation</label>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="0.4" />
+                        </div>
+                        <div>
+                            <label>Green Spaces</label>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="0.1" />
+                        </div>
+                        <button type="submit" value="Submit" className="btn btn-primary">Submit</button>
+                     </form>
+                </div>
+          </div>
+        );
+      }
+}
+
+export default Sidebar;
