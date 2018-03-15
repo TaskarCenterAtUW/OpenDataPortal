ac0c7dc7b3d24796fb18e9183fd24f036cfee31d
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-03-15 12:22:51 -0700

    Running the csv file on a seperate local server

--- dashboard/src/getData.js
+++ dashboard/src/getData.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 
 function getData() {
-    d3.csv("Data/uw_ped_data.csv", (data) => {
+    d3.csv("http://localhost:8000/uw_ped_data.csv", (data) => {
         console.log(data);
     });
 }
