0e53026c41ea038774e0b62dfc50a4c10be2029c
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-03-15 11:45:10 -0700

    Added starter code to get data

--- dashboard/src/getData.js
+++ dashboard/src/getData.js
@@ -1,3 +1,9 @@
-export default {
-    
+import * as d3 from "d3";
+
+function getData() {
+    d3.csv("Data/uw_ped_data.csv", (data) => {
+        console.log(data);
+    });
 }
+
+export {getData};
