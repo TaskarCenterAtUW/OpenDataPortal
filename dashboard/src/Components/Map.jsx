4a7fce1ac48053f74294428bf4f01a559ded7e7d
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-06-08 11:22:58 -0700

    Added code to filter sidewalks close to the pin

--- dashboard/src/Components/Map.jsx
+++ dashboard/src/Components/Map.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from 'mapbox-gl-geocoder';
-import { parseData } from '../getData';
-import { result } from '../getData';
+const GeoJSON = require('geojson');
+// This is only contains partial data
+const DataRepo = require('./../Data/data.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFuZXNoc3UiLCJhIjoiY2pkamZpbzZ3MW0ycTJ6cmxtNnJhZ2k4ZCJ9.bzJJ_dSQlZefW6kWSSjlzw';
 var map;
@@ -11,16 +12,6 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {}
-  }
-
-  getData() {
-    parseData();
-        setTimeout(() => {
-            console.log(result["collection"]);
-            this.setState({
-                collection: result["collection"]
-            });
-        }, 100);
   }
 
   componentWillMount() {
@@ -48,13 +39,13 @@ class Map extends Component {
     map.addControl(new mapboxgl.NavigationControl());
 
     map.on('load', () => {
-      
+
       //Point placed after search
       map.addSource("point", {
         "type": "geojson",
         "data": {
-            "type": "FeatureCollection",
-            "features": []
+          "type": "FeatureCollection",
+          "features": []
         }
       });
 
@@ -63,15 +54,15 @@ class Map extends Component {
         "source": "point",
         "type": "circle",
         "paint": {
-            "circle-radius": 10,
-            "circle-color": "red"
+          "circle-radius": 10,
+          "circle-color": "red"
         }
       });
 
       var data = require('./sidewalks.geojson')
 
-      map.addSource('sidewalks', { 
-        "type": "geojson", 
+      map.addSource('sidewalks', {
+        "type": "geojson",
         "data": data
       });
 
@@ -87,32 +78,57 @@ class Map extends Component {
           "line-color": "red",
           "line-width": 3,
           "opacity": this.props.crime
-      }
-    });
+        }
+      });
 
-      //Change point after search
-      geocoder.on('result', function(ev) {
-          map.getSource('point').setData(ev.result.geometry);
-          parseData();
-          /*result.forEach((row) => {
-            map.addLayer({
-              "id": "route",
-              "type": "line",
-              "source": {
-                  "type": "geojson",
-                  "data": row.geometry
-              },
-              "layout": {
-                  "line-join": "round",
-                  "line-cap": "round"
-              },
-              "paint": {
-                  "line-color": "red",
-                  "line-width": 8,
-                  'line-opacity': 0.5
-              }
-            });
-          })*/
+      // Change point after search
+      geocoder.on('result', function (ev) {
+        map.getSource('point').setData(ev.result.geometry);
+        let pin = ev.result.geometry;
+        // hard code the pin
+        pin = [-122.4019, 47.673490];
+        // console.log(pin);
+        let sidewalks = DataRepo["default"]["data"]["collection"]["features"];
+        // console.log(sidewalks[0]);
+        let relSidewalks = [];
+        sidewalks.forEach((elem) => {
+          let coords = elem["geometry"]["coordinates"];
+          let maxLong = Math.max(coords[0][0], coords[1][0]);
+          let minLong = Math.min(coords[0][0], coords[1][0]);
+          let maxLat = Math.max(coords[0][1], coords[1][1]);
+          let minLat = Math.min(coords[0][1], coords[1][1]);
+          // check if the sidewalk is close to the point
+          if (((pin[0] < maxLong) && (pin[0] > minLong)) && ((pin[1] < maxLat) && (pin[1] > minLat))) {
+            relSidewalks.push(elem);
+          }
+        });
+        
+        
+        console.log(relSidewalks);
+        /*
+        DataRepo.forEach((elem) => {
+
+        });
+        */
+        /*result.forEach((row) => {
+          map.addLayer({
+            "id": "route",
+            "type": "line",
+            "source": {
+                "type": "geojson",
+                "data": row.geometry
+            },
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round"
+            },
+            "paint": {
+                "line-color": "red",
+                "line-width": 8,
+                'line-opacity': 0.5
+            }
+          });
+        })*/
       });
     });
   }
